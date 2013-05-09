abed1b5972bf729f30ac0cc5c56056718812081d
Sydney Bennett <sbennett@lancope.com>
2013-05-09 10:04:06 -0400

    variable changes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 
 (function(){
 	console.log("hello");
-	var x = 9;
-	alert("hello again");
-	
-	if (x < 10){
+	var k = 9;
+	alert("hello again!");
+	if (k < 10){
 		console.log ("it's small!");
 	}
 })();
